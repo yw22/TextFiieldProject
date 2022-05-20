f19033732b1528e47d3dd40c85f675b8eaedaf3f
yw22 <youngwook.dev@gmail.com>
2022-05-20 19:40:09 +0900

    feat : "텍스트필드 구현"
    델리게이트 패턴사용

--- TextFiieldProject/ViewController.swift
+++ TextFiieldProject/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+        
+        setup()
+        
     }
-
+    
+    func setup() {
+        view.backgroundColor = UIColor.gray
+        
+        textField.keyboardType =  UIKeyboardType.emailAddress
+        textField.placeholder = "이메일을 입력해주세요"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .done
+        
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트필드의 입력을 시작했다.")
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        print(#function)
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+        
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+        print("호출이 끝났습니다.")
+    }
+    
+    
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
 
 }
 
